c7786e92167faf727c685cc564a206bbddb7c23e
Zens <95537796+zz3ns@users.noreply.github.com>
2023-12-20 22:52:02 -0300

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,9 +1,3 @@
-// function toggleNavMobile() {
-//     var navbarDropdown = document.getElementById('navbar-dropdown');
-
-//     navbarDropdown.css('display', 'block');
-// };
-
 $(document).ready(function () {
     $('.navbar-mobile').on('click', function () {
         $('.navbar-dropdown').toggleClass('active-dropdown');
